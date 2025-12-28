b1a5245064917a3931d3b6044711065a9f4dce23
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-12-28 23:22:48 +0000

    Update static_root in settings.py

--- the_newshub/settings.py
+++ the_newshub/settings.py
@@ -172,9 +172,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
