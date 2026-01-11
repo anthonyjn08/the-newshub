6093565285b4db87a180d8a5cae9c9e2e236d4ea
Anthony Nelson <anthonyjnelson@hotmail.com>
2026-01-11 16:10:07 +0000

    Re add gmail config to settings.py

--- the_newshub/settings.py
+++ the_newshub/settings.py
@@ -166,10 +166,19 @@ AUTH_PASSWORD_VALIDATORS = [
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 DEFAULT_FROM_EMAIL = "thenewhub@resend.dev"
 
-# Email backend for development (terminal output)
+# Email
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "noreply.thenewshub@gmail.com"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
